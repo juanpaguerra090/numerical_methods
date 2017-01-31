1cb8605f52b7195c922a4124891df1ba6db0e23f
Juan Pablo Ramirez <juanpaguerra090@hotmail.com>
2017-01-31 22:01:52 +0000

    Updated Bisection.cc

--- bisection.cc
+++ bisection.cc
@@ -1,47 +1,44 @@
-using std::cout;
-using std::endl;
-#include <string>
+using namespace std;
 #include <iostream>
 #include <cmath>
 
-
-float function(float x){
-    return (2*pow(x,3))-(5*pow(x,2))+(3*x);
+float function (float x){
+    return (2*pow(x,4) + pow(x,3) - 8*pow(x,2) - x + 6)/(x+2);
 }
 
-float raiz(float x1, float x2){
+float root(float x1, float x2){
+    float l = function(x1);
+    float r = function(x2);
+    float mid = (x1+x2)/2;
     
-    float left=function(x1);
-    float right=function(x2);
-    float middle=(x1+x2)/2;
-
-    if(function(middle)==0){
-        return middle;
+    if (function(mid)==0){
+        return mid;
     }
+    
     else if(x1==x2){
         return x1;
     }
-    else if(left*right<0){
-        if(left>=-0.0000000000000001 && left<=0.0000000000000001 && right>=-0.0000000000000001 && right<=0.0000000000000001){
-            return middle;
-        }
+    
+    else if (l*r<0){
+        if (l >= -.0000000000001 && l <= .0000000000001 && r >= -.0000000000001 && r <= .0000000000001){
+            return mid;
+        } 
         else{
-            if((left*function(middle))<0){
-                return raiz(x1,middle);
+            if(l*function(mid)<0){
+                return root(x1, mid);
             }
-            else if((function(middle)*right)<0){
-                return raiz(middle,x2);
+            else if((function(mid)*r)<0){
+                return root(mid, x2);
             }
         }
-        std::cout « " yup. ";
+        std::cout<< ": yup";
     }
     else{
-        std::cout « std::endl « " Intersection not found. There might be more than 1.";
-        return -999;
+        std::cout << std::endl <<"intersection not found, there must be more than 1";
+        return -1;
     }
 }
 
-int main() {
-    std::cout « " Hello World!" « std::endl
-    std::cout  « std::endl « "raiz = " « raiz(0,2);
+int main (){
+    std::cout << std::endl << "root: "<< root(-4,0);
 }
